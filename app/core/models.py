a79b779119c026c7139771df8856dfdb3e078b45
Lucas Medeiros <lucas.medeiros@loadsmart.com>
2023-04-13 16:30:18 -0300

    Docs and user app

---
+++ app/core/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        if not email:
+            raise ValueError("The user must have an email address")
+
+        user = self.model(email=email.lower(), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        if not email:
+            raise ValueError("The user must have an email address")
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,28 +20,25 @@ class ModelTests(TestCase):
         """Test email is normalized for new users"""
 
         sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['test1@EXAMPLE.COM', 'test1@example.com'],
-            ['TeSt1@examplE.com', 'test1@example.com'],
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["test1@EXAMPLE.COM", "test1@example.com"],
+            ["TeSt1@examplE.com", "test1@example.com"],
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEquals(user.email, expected)
             get_user_model().delete(user)
 
     def test_new_user_without_email_raises_error(self):
         """Test user creation without email"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test1234')
+            get_user_model().objects.create_user("", "test1234")
 
     def test_create_superuser(self):
         """Test creating superuser"""
-        user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test1234'
-        )
+        user = get_user_model().objects.create_superuser("test@example.com", "test1234")
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
